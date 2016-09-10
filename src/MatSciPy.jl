22a252e88557d346210d1c7a10df0ad40dcfd18c
Christoph Ortner <c.ortner@warwick.ac.uk>
2016-09-10 22:00:05 +0100

    rewrote the matscipy neighbourlist interface to pass all arrays by reference now instead of copying them

--- src/MatSciPy.jl
+++ src/MatSciPy.jl
@@ -8,11 +8,12 @@ Julia wrappers for the [matscipy](https://github.com/libAtoms/matscipy) library.
 At the moment, only the neighbourlist is wrapped, which is important for
 fast neighbour calculations. `matscipy` depends on `ASE`, hence `MatSciPy.jl`
 is a sub-module of `ASE.jl`.
-For the most part it remains shielded from the user: if `ASE` detects that
-`matscipy` is installed it will use `MatSciPy.NeighbourList`, otherwise it
-will use `ASE.NeighbourList`. (TODO)
 
-* `JuLIP.ASE.MatSciPy.neighbour_list` : the raw neighbour_list wrapper
+This module ought to remain shielded from a user, calling
+`neighbourlist(at)` for an object `at::ASEAtoms` should automatically
+generate a `MatSciPy` neighbourlist.
+
+* `JuLIP.ASE.MatSciPy.__neighbour_list__` : the raw neighbour_list wrapper
 * `JuLIP.ASE.MatSciPy.NeighbourList` : boiler plate type, including iterators
 """
 module MatSciPy
@@ -28,6 +29,9 @@ import JuLIP: sites, bonds
 import Base: start, done, next
 
 
+# renamed neighbour_list to __neighbour_list__ to make it clear this is
+# an internal function now; this is due to the fact that we don't
+# copy the neighbourlist arrays anymore
 """
 `neighbour_list(atoms::ASEAtoms, cutoff::Float64, quantities::AbstractString)`
 `neighbour_list(atoms::ASEAtoms, cutoff::Float64)`
@@ -46,50 +50,34 @@ By convention, the string *must* start with `"ij"`.
 distance vectors `D` from the ASE N x 3 to the Atoms.jl 3 x N convention!
 Use the kwarg `convertarrays=true` to do so.
 """
-function neighbour_list(atoms::ASEAtoms,
+function __neighbour_list__(atoms::ASEAtoms,
                         cutoff::Float64,
                         quantities="ijdDS";
                         convertarrays=true)
-   results = matscipy_neighbours.neighbour_list(quantities,
-                                                pyobject(atoms),
-                                                cutoff)
-   results2 = pycall(matscipy_neighbours.neighbour_list,
+   # compute the neighbourlist via matscipy, get the data as
+   # PyArrays, i.e., just references, no copies
+   results = pycall(matscipy_neighbours.neighbour_list,
                      NTuple{length(quantities), PyArray}, quantities,
                      pyobject(atoms), cutoff)
-   if length(quantities) == 1; results = (results,); end
-   results = collect(Any, results)
-   results2 = collect(Any, results2)
-
-   for ii = 1:3
-      @assert results[ii] == pyarrayref(results2[ii], own=true)
-   end
-
-   R = results[4]
-   R2 = results2[4]
-   @show typeof(R), size(R)
-   @show typeof(R2), size(R2)
-   @assert R' == pyarrayref(R2, own=true)
-   @assert results[5]' == pyarrayref(results2[5], own=true)
-   println("Assertions passed")
-   quit()
-
+   # create Julia arrays referencing the same memory
+   jresults = [pyarrayref(r) for r in results]
+   # fix the arrays for later use
    for (idx, quantity) in enumerate(quantities)
-      # convert the PyArray to a Julia Array
-      # results[idx] = pyarrayref(results[idx], own=true)
-      # convert indices to 1-based (Julia convention)
+      # convert indices to 1-based (Julia is 1-based, python 0-based)
       if (quantity == 'i' || quantity == 'j')
-         results[idx][:] += 1
-         # @simd for n = 1:length(results[idx])
-         #    @inbounds results[idx][n] += 1
-         # end
+         r_idx = jresults[idx]::Vector{Int32}
+         @simd for n = 1:length(r_idx)
+            @inbounds r_idx[n] += 1
+         end
       end
-      # convert R ans S matrices to arrays of vectors
+      # convert R and S matrices to arrays of vectors
       if convertarrays
-         if quantity == 'D'; results[idx] = vecs(results[idx]'); end
-         if quantity == 'S'; results[idx] = vecs(results[idx]'); end
+         if quantity == 'D'; jresults[idx] = vecs(jresults[idx]); end
+         if quantity == 'S'; jresults[idx] = vecs(jresults[idx]); end
       end
    end
-   return tuple(results...)
+   @show typeof(tuple(jresults..., results))
+   return tuple(jresults..., results)
 end
 
 
@@ -110,11 +98,14 @@ type NeighbourList <: AbstractNeighbourList
     r::Vector{Float64}
     R::JVecs{Float64}
     S::JVecs{Int32}
+    pyarrays
 end
+# the last field is a hack to make sure the python arrays are not freed
+# TODO: should study this again and maybe file an issue with PyCall
 
 # default constructor from an ASEAtoms object
 NeighbourList(at::ASEAtoms, cutoff::Float64) =
-   NeighbourList(cutoff, neighbour_list(at, cutoff, convertarrays=true)... )
+   NeighbourList(cutoff, __neighbour_list__(at, cutoff)... )
 
 import Base.length
 length(nlist::NeighbourList) = length(nlist.i)
