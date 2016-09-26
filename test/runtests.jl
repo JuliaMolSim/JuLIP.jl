a19b4fbbdaa56560cf80f10883544bc8d2cbe8b0
Christoph Ortner <c.ortner@warwick.ac.uk>
2016-09-26 16:56:20 +0100

    tests pass; now for the real implementation.

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,31 +39,26 @@ set_constraint!(at, VariableCell(at))
 # gradient(at, x)
 # JuLIP.Testing.fdtest(calc, at, verbose=true, rattle=false)
 
-function polar(F::Matrix)
-   @assert size(F,1) == size(F,2)
-   Q1, D, Q2 = svd(F)
-   return Q1 * Q2', Q2 * diagm(D) * Q2'
-end
 
 # deform cell a bit
-X = positions(at)
-F = cell(at)
-dF = eye(3,3) + rand(3,3) * 0.1
-# dF = [1.0 0.1 0.0; 0.1 1.0 0.0; 0.0 0.0 1.0]
-set_cell!(at, dF * F * dF')
-set_positions!(at,  [JVec(dF*F*dF' * (F \ x)) for x in X] )
+X0 = positions(at)
+F = cell(at)'
+dF = eye(3) + 0.1 * rand(3,3)
+set_cell!(at, (dF*F)')
+set_positions!(at,  [JVec(dF * x) for x in X0])
 # create non-zero forces
 # X = positions(at)
 # X[1] += 0.1
 # set_positions!(at, X)
 rattle!(at, 0.1)
 @show maxnorm(forces(at))
-@show cell(at)
+@show cell(at)'
+println("...............................")
 
-F0 = cell(at)
-F = copy(F0)
+F0 = cell(at)'
+F = copy(F0)'
 X0 = positions(at)
-∂W = (stress(at) |> Array)  / F'
+∂W = (stress(at) |> Array)  / F
 E0 = energy(at)
 ∂Wh = zeros(∂W)
 for p = 3:7
@@ -71,10 +66,9 @@ for p = 3:7
    for i = 1:9
       U = zeros(3,3); U[i] += h
       F = Matrix(F0 + U)
-      Q, F = polar(F)
+      set_cell!(at, F')
       A = JMat( F * pinv(F0) )
       X = [ A * x for x in X0 ]
-      set_cell!(at, F)
       set_positions!(at, X)
       ∂Wh[i] = (energy(at) - E0) / h
    end
@@ -83,29 +77,3 @@ end
 
 @show ∂W
 @show ∂Wh
-
-
-# # check why rotation changes energy
-#
-# E0 = energy(at)
-#
-# # generate a rotation matrix
-# U = [0 1.0 0; -1.0 0 0; 0 0 0]
-# h = 0.1
-# Q = expm(h * U)
-# @assert abs(det(Q) - 1.0) < 1e-10
-# @assert vecnorm(Q'*Q - eye(3)) < 1e-10
-#
-# # deform cell
-# F0 = cell(at)
-# X0 = positions(at)
-# F = Q * F0
-# X = [ JMat(Q) * x for x in X0 ]
-# set_cell!(at, F)
-# set_positions!(at, X)
-#
-# # compute energy after rotation
-# Eh = energy(at)
-# @show E0, Eh
-# # output
-# #    (E0,Eh) = (-126.23143471478076,-112.77456617599405)
