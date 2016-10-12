df495881bb709f4d73b65478861accf32cbe6ddb
Christoph Ortner <c.ortner@warwick.ac.uk>
2016-10-12 07:03:53 +0100

    new stilling weber implemented and  tested

--- src/stillingerweber.jl
+++ src/stillingerweber.jl
@@ -1,15 +1,29 @@
 
-# this is a quick and dirty implementation of the Stillinger-Weber model;
-# in the future it will be good to do this more elegantly.
-
-# Model and model paramters taken from
-# https://quantumwise.com/documents/manuals/ATK-2014/ReferenceManual/index.html/ref.stiwe3potential.html
-# which seem to be in the right units; should probably get a better reference
-# and make sure the translation of units is indeed correct
-
-# NOTE: there is huge overhead in this code that can be
+# NOTE: this is a quick and dirty implementation of the Stillinger-Weber model;
+# in the future it will be good to do this more elegantly / more efficiently
+# there is huge overhead in this code that can be
 #   significantly improved in terms of performance, but for now we just
 #   want a correct and readable code
+
+# Functional Form from [Stillinger/Weber, PRB 1985]
+# ---------------------------------------------------
+# v2 = ϵ f2(r_ij / σ)
+# v3 = ϵ f3(ri/σ, rj/σ, rk/σ)
+# f2(r) = A (B r^{-p} - r^{-q}) exp( (r-a)^{-1} )
+# f3 = h(rij, rij, Θjik) + ... + ...
+# h(rij, rik, Θjik) = λ exp[ γ (rij-a)^{-1} + γ (rik-a)^{-1} ] * (cos Θjik + 1/3)^2
+#
+# V2 = 0.5 * ϵ * A (B r^{-p} - r^{-q}) * exp( (r-a)^{-1} )
+# V3 = √ϵ * λ exp[ γ (r-a)^{-1} ]
+#
+#
+# Parameters from QUIP database:
+# -------------------------------
+# <per_pair_data atnum_i="14" atnum_j="14" AA="7.049556277" BB="0.6022245584"
+#           p="4" q="0" a="1.80" sigma="2.0951" eps="2.1675" />
+# <per_triplet_data atnum_c="14" atnum_j="14" atnum_k="14"
+#               lambda="21.0" gamma="1.20" eps="2.1675" />
+
 
 
 export StillingerWeber
@@ -17,15 +31,16 @@ export StillingerWeber
 import JuLIP.Potentials: evaluate, evaluate_d, @pot, @D
 
 
-bondangle(R1, R2) = 0.5 * ( dot(R1/norm(R1), R2/norm(R2)) + 1.0/3.0 )^2
+iprhat(R1, R2) = dot(R1/norm(R1), R2/norm(R2))
+
+bondangle(R1, R2) = 0.5 * (iprhat(R1, R2) + 1.0/3.0)^2
 
 function bondangle_d(R1, R2)
    d = dot(R1/norm(R1), R2/norm(R2))
    b1 = R2 / (norm(R2)*norm(R1)) - (d/norm(R1)^2) * R1
    b2 = R1 / (norm(R1)*norm(R2)) - (d/norm(R2)^2) * R2
-   return (d+1./3.)*b1, (d+1./3.)*b2
+   return 0.5*(d+1./3.)^2, (d+1./3.)*b1, (d+1./3.)*b2
 end
-
 
 
 @pot type StillingerWeber{P1,P2} <: SitePotential
@@ -41,13 +56,15 @@ StillingerWeber
 
 cutoff(calc::StillingerWeber) = max(cutoff(calc.V2), cutoff(calc.V3))
 
-StillingerWeber(; p=4.0, A = 15.2855528754, B = 0.60222456,
-                  cutoff = 3.77118, γ2 = 2.0951, γ3 = 2.51412, λ = 45.5343 ) =
+StillingerWeber(; ϵ=2.1675, σ = 2.0951, A=7.049556277, B=0.6022245584,
+                  p = 4, a = 1.8, λ=21.0, γ=1.20 ) =
    StillingerWeber(
-      AnalyticPotential(:( $A * ($B * r^(-$p) - 1.0) )) * SWCutoff(Rc=cutoff, Lc=γ2),
-      SWCutoff(Rc=cutoff, Lc=γ3, e0=2.0*sqrt(λ))
+      AnalyticPotential(:( $(0.5 * ϵ * A) * ($B * (r/$σ)^(-$p) - 1.0)
+                                 * exp( 1.0 / (r/$σ - $a) ) ),
+                        cutoff = a*σ-1e-3),
+      AnalyticPotential(:( $(sqrt(ϵ) * λ) * exp( $γ / (r/$σ - $a) ) ),
+                        cutoff = a*σ-1e-3)
    )
-
 
 function evaluate(calc::StillingerWeber, r, R)
    Es = 0.0
@@ -68,8 +85,7 @@ function evaluate_d(calc::StillingerWeber, r, R)
    dEs = [ (evaluate_d(calc.V2, ri) / ri) * Ri for (ri, Ri) in zip(r, R) ]
    # three-body terms
    for i1 = 1:(length(r)-1), i2 = (i1+1):length(r)
-      a = bondangle(R[i1], R[i2])
-      b1, b2 = bondangle_d(R[i1], R[i2])
+      a, b1, b2 = bondangle_d(R[i1], R[i2])
       V1 = calc.V3(r[i1])
       V2 = calc.V3(r[i2])
       dV1 = (@D calc.V3(r[i1])) * R[i1] / r[i1]
