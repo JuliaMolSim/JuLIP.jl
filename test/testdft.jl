a9495b20155d7faf28211bc80d00dfe7e5958235
James Kermode <james.kermode@gmail.com>
2016-11-09 21:17:46 +0000

    change gauge for KE integration. now sum(site_energies()) == energy()

--- test/testdft.jl
+++ test/testdft.jl
@@ -10,9 +10,24 @@ using JuLIP.DFT
 # ======================================================================
 
 at = bulk("Si", cubic=true)
-g = GPAWCalculator()
+g = GPAWCalculator(xc="LDA",
+                   gpts=(32, 32, 32),
+                   nbands="150%",
+                   kpts=(1, 1, 1),
+                   occupations=FermiDirac(width=0.01),
+		   convergence=Dict("energy" => 1e-7,
+                                    "eigenstates" => 1e-10,
+                                    "density" => 1e-7))
 println(g)
 set_calculator!(at, g)
 
-eden = energy_density(g, at)
-E_a = site_energies(g, at)
+E = energy(g, at)
+println("E = ", E)
+
+E_a = site_energies(g,at)
+println("E_a = ", E_a)
+println("sum(E_a) = ", sum(E_a))
+println("E - sum(E_a) = ", E - sum(E_a))
+
+@test (E - sum(E_a)) < 1e-3
+
