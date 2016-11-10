68e5447fe82d45f890dba1ec2ed2ef8bb9c46470
James Kermode <james.kermode@gmail.com>
2016-11-10 10:43:05 +0000

    minor style fixes to DFT.jl; inclusion in test set if GPAW installed

--- test/testdft.jl
+++ test/testdft.jl
@@ -9,25 +9,26 @@ using JuLIP.DFT
 
 # ======================================================================
 
-at = bulk("Si", cubic=true)
-g = GPAWCalculator(xc="LDA",
-                   gpts=(32, 32, 32),
-                   nbands="150%",
-                   kpts=(1, 1, 1),
-                   occupations=FermiDirac(width=0.01),
-		   convergence=Dict("energy" => 1e-7,
-                                    "eigenstates" => 1e-10,
-                                    "density" => 1e-7))
-println(g)
-set_calculator!(at, g)
+if gpaw_available
+    at = bulk("Si", cubic=true)
+    g = GPAWCalculator(xc="LDA",
+                       gpts=(32, 32, 32),
+                       nbands="150%",
+                       kpts=(1, 1, 1),
+                       occupations=FermiDirac(width=0.01),
+    		   convergence=Dict("energy" => 1e-7,
+                                        "eigenstates" => 1e-10,
+                                        "density" => 1e-7))
+    println(g)
+    set_calculator!(at, g)
 
-E = energy(g, at)
-println("E = ", E)
+    E = energy(g, at)
+    println("E = ", E)
 
-E_a = site_energies(g,at)
-println("E_a = ", E_a)
-println("sum(E_a) = ", sum(E_a))
-println("E - sum(E_a) = ", E - sum(E_a))
+    E_a = site_energies(g,at)
+    println("E_a = ", E_a)
+    println("sum(E_a) = ", sum(E_a))
+    println("E - sum(E_a) = ", E - sum(E_a))
 
-@test (E - sum(E_a)) < 1e-3
-
+    @test (E - sum(E_a)) < 1e-3
+end
