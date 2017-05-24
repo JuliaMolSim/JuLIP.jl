e0fc71f69d5c49c673587103a6e0de244c058f8f
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-05-24 14:57:38 +0100

    added an AD hessian test into admit

--- src/adsite.jl
+++ src/adsite.jl
@@ -78,3 +78,55 @@ end
 #    d = _RD_.gradient( r_ -> ad_evaluate(pot, r_), collect(r) )
 #    return [ (d[i]/r[i]) * R[i] for i = 1:length(r) ]
 # end
+
+
+
+
+####### Prototype AD Hessian Implementation
+# using JuLIP, JuLIP.Potentials
+# using JuLIP.Potentials: evaluate_d
+# JJ = JuLIP
+# JPP = JuLIP.Preconditioners
+#
+# function ad_grad(V::SitePotential, S)
+#    R = reshape(S, 3, length(S) ÷ 3) |> vecs
+#    r = [norm(u) for u in R]
+#    dV = evaluate_d(V, r, R)
+#    return mat(dV)[:]
+# end
+#
+# ad_hess(V::SitePotential, S::Vector{Float64}) = ForwardDiff.jacobian( S_ -> ad_grad(V, S_), S )
+#
+# ad_hess(V, R::AbstractVector{JVecF}) = ad_hess(V, mat(R)[:])
+#
+# atind2lininds(i::Integer) = (i-1) * 3 + [1,2,3]
+#
+# localinds(j::Integer) =  3 * (j-1) + [1,2,3]
+#
+# function ad_hessian(V, at::AbstractAtoms)
+#    I = Int[]; J = Int[]; Z = Float64[]
+#    for (i0, neigs, r, R, _) in sites(at, cutoff(V))
+#       ii = atind2lininds(i0)
+#       jj = [atind2lininds(j_) for j_ in neigs]
+#       # compute positive version of hessian of V(R)
+#       hV = ad_hess(V, R)
+#
+#       nneigs = length(neigs)
+#       for j1 = 1:nneigs, j2 = 1:nneigs
+#          LJ1, LJ2 = localinds(j1), localinds(j2)       # local indices
+#          GJ1, GJ2 = jj[j1], jj[j2]                     # global indices
+#          H = view(hV, LJ1, LJ2)
+#          for a = 1:3, b = 1:3
+#             append!(I, [ GJ1[a],    ii[a],  GJ1[a],  ii[a] ])
+#             append!(J, [ GJ2[b],   GJ2[b],   ii[b],  ii[b] ])
+#             append!(Z, [ H[a,b], - H[a,b], -H[a,b], H[a,b] ])
+#          end
+#       end
+#    end
+#    N = 3*length(at)
+#    return sparse(I, J, Z, N, N)
+# end
+#
+# ad_hessian(at::AbstractAtoms, x::Vector) =
+#     ad_hessian(calculator(at), set_dofs!(at, x))
+#
