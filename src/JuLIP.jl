ed291fe4c33b0ed85fa4c4dd4b0bacf2ee503d9d
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-07-23 00:12:41 +0100

    setting up automatic build process

--- src/JuLIP.jl
+++ src/JuLIP.jl
@@ -5,69 +5,61 @@ module JuLIP
 
 using PyCall, Reexport
 
-_found_ase_ = false
-_found_matscipy_ = false
-include("ext.jl")
-
-
 export Atoms
 
 # TODO: correctly use import versus using throughout this package!
 
-if _found_ase_ && _found_matscipy_
+# quickly switch between Matrices and Vectors of Vecs
+include("arrayconversions.jl")
 
-   # quickly switch between Matrices and Vectors of Vecs
-   include("arrayconversions.jl")
+# define types and abstractions of generic functions
+include("abstractions.jl")
 
-   # define types and abstractions of generic functions
-   include("abstractions.jl")
+# a few auxiliary routines
+include("utils.jl")
 
-   # a few auxiliary routines
-   include("utils.jl")
+# implementation of some key functionality via ASE
+include("ASE.jl")
+@reexport using JuLIP.ASE
 
-   # implementation of some key functionality via ASE
-   include("ASE.jl")
-   @reexport using JuLIP.ASE
+# define the default atoms object
+"""
+`type Atoms`
 
-   # define the default atoms object
-   """
-   `type Atoms`
+Technically not a type but a type-alias, to possibly allow different "backends".
+At the moment, `Atoms = ASE.ASEAtoms`; see its help for more details.
+This will likely remain for the foreseeable future.
+"""
+typealias Atoms ASE.ASEAtoms
 
-   Technically not a type but a type-alias, to possibly allow different "backends".
-   At the moment, `Atoms = ASE.ASEAtoms`; see its help for more details.
-   This will likely remain for the foreseeable future.
-   """
-   typealias Atoms ASE.ASEAtoms
-
-   # only try to import Visualise, it is not needed for the rest to work.
-   try
-      # some visualisation options
-      if isdefined(Main, :JULIPVISUALISE)
-         if Main.JULIPVISUALISE == true
-            include("Visualise.jl")
-         end
+# only try to import Visualise, it is not needed for the rest to work.
+try
+   # some visualisation options
+   if isdefined(Main, :JULIPVISUALISE)
+      if Main.JULIPVISUALISE == true
+         include("Visualise.jl")
       end
-   catch
-      JuLIP.julipwarn("""JuLIP.Visualise did not import correctly, probably because
-                  `imolecule` is not correctly installed.""")
    end
-
-   # interatomic potentials prototypes and some example implementations
-   include("Potentials.jl")
-
-   # submodule JuLIP.Constraints
-   include("Constraints.jl")
-   @reexport using JuLIP.Constraints
-
-   # basic preconditioning capabilities
-   include("preconditioners.jl")
-
-   # some solvers
-   include("Solve.jl")
-   @reexport using JuLIP.Solve
-
-   # codes to facilitate testing
-   include("Testing.jl")
+catch
+   JuLIP.julipwarn("""JuLIP.Visualise did not import correctly, probably because
+               `imolecule` is not correctly installed.""")
 end
+
+# interatomic potentials prototypes and some example implementations
+include("Potentials.jl")
+
+# submodule JuLIP.Constraints
+include("Constraints.jl")
+@reexport using JuLIP.Constraints
+
+# basic preconditioning capabilities
+include("preconditioners.jl")
+
+# some solvers
+include("Solve.jl")
+@reexport using JuLIP.Solve
+
+# codes to facilitate testing
+include("Testing.jl")
 
 end # module
