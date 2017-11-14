e0651a90f7a940b40fe196b2179e0e46d113290d
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-11-14 12:59:29 +0000

    added eam, sw to performance tests

--- test/performance.jl
+++ test/performance.jl
@@ -1,21 +1,46 @@
 using BenchmarkTools
 using JuLIP, JuLIP.ASE, JuLIP.Potentials
 
+println()
 println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
 println("   JuLIP Performance Regression Tests")
 println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
 println()
+
+println("--------------------------------------------------------------------------")
 println("LENNARD-JONES")
-println("==============")
+const lj = lennardjones(r0=JuLIP.ASE.rnn("Al"))
+const al = bulk("Al", cubic=true) * (10,10,8)
 
-calc = lennardjones(r0=JuLIP.ASE.rnn("Al"))
-at = bulk("Al", cubic=true) * (20,20,2)
-set_calculator!(at, calc)
-set_constraint!(at, FixedCell(at))
+print("Energy Assembly (without nlist): ")
+@btime energy($lj, $al)
 
-println("Energy Assembly (without nlist)")
-@btime energy($calc, $at)
+print("Energy Assembly (with nlist):    ")
+f = () -> energy(lj,  rattle!(al, 0.001))
+@btime f()
 
-println("Energy Assembly (with nlist)")
-f() = energy(calc,  rattle!(at,0.001))
+println("--------------------------------------------------------------------------")
+println("EAM (Splines)")
+data = joinpath(dirname(@__FILE__), "..", "data") * "/"
+const eam_Fe = JuLIP.Potentials.EAM(data * "pfe.plt", data * "ffe.plt", data * "F_fe.plt")
+const fe = bulk("Fe", cubic=true) * (12,12,8)
+
+print("Energy Assembly (without nlist): ")
+@btime energy($eam_Fe, $fe)
+
+print("Energy Assembly (with nlist):    ")
+f = () -> energy(eam_Fe,  rattle!(fe, 0.001))
+@btime f()
+
+
+println("--------------------------------------------------------------------------")
+println("STILLINGER-WEBER")
+const sw = StillingerWeber()
+const si = bulk("Si", cubic=true) * (12,15,12)
+
+print("Energy Assembly (without nlist): ")
+@btime energy($sw, $si)
+
+print("Energy Assembly (with nlist):    ")
+f = () -> energy(sw,  rattle!(si, 0.001))
 @btime f()
