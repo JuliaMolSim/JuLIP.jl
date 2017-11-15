999cb528b3decca6e8f1386bb50752e7d28ae1b5
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-11-15 23:24:12 +0000

    modified performance regression tests

--- test/performance.jl
+++ test/performance.jl
@@ -1,5 +1,28 @@
 using BenchmarkTools
 using JuLIP, JuLIP.ASE, JuLIP.Potentials
+using JuLIP.Potentials: EAM
+
+function perfbm(id::AbstractString, at::AbstractAtoms, calc::AbstractCalculator;
+                e = true, elist = true, f = true, flist = true)
+   println("--------------------------------------------------------------------------")
+   println(id)
+   if e
+      print("Energy Assembly (without nlist): ")
+      @btime energy($calc, $at)
+   end
+   if elist
+      print("Energy Assembly (with nlist):    ")
+      @btime energy($calc,  rattle!($at, 0.001))
+   end
+   if f
+      print("Force Assembly (without nlist):  ")
+      @btime forces($calc, $at)
+   end
+   if flist
+      print("Force Assembly (with nlist):     ")
+      @btime forces($calc,  rattle!($at, 0.001))
+   end
+end
 
 println()
 println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
@@ -7,40 +30,15 @@ println("   JuLIP Performance Regression Tests")
 println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
 println()
 
-println("--------------------------------------------------------------------------")
-println("LENNARD-JONES")
-const lj = lennardjones(r0=JuLIP.ASE.rnn("Al"))
-const al = bulk("Al", cubic=true) * (10,10,8)
+perfbm("LENNARD-JONES",
+         bulk("Al", cubic=true) * (10,10,8),
+         lennardjones(r0=JuLIP.ASE.rnn("Al")) )
 
-print("Energy Assembly (without nlist): ")
-@btime energy($lj, $al)
-
-print("Energy Assembly (with nlist):    ")
-f = () -> energy(lj,  rattle!(al, 0.001))
-@btime f()
-
-println("--------------------------------------------------------------------------")
-println("EAM (Splines)")
 data = joinpath(dirname(@__FILE__), "..", "data") * "/"
-const eam_Fe = JuLIP.Potentials.EAM(data * "pfe.plt", data * "ffe.plt", data * "F_fe.plt")
-const fe = bulk("Fe", cubic=true) * (12,12,8)
+perfbm("EAM (Splines)",
+         bulk("Fe", cubic=true) * (12,12,8),
+         EAM(data * "pfe.plt", data * "ffe.plt", data * "F_fe.plt") )
 
-print("Energy Assembly (without nlist): ")
-@btime energy($eam_Fe, $fe)
-
-print("Energy Assembly (with nlist):    ")
-f = () -> energy(eam_Fe,  rattle!(fe, 0.001))
-@btime f()
-
-
-println("--------------------------------------------------------------------------")
-println("STILLINGER-WEBER")
-const sw = StillingerWeber()
-const si = bulk("Si", cubic=true) * (10,10,12)
-
-print("Energy Assembly (without nlist): ")
-@btime energy($sw, $si)
-
-print("Energy Assembly (with nlist):    ")
-f = () -> energy(sw,  rattle!(si, 0.001))
-@btime f()
+perfbm("STILLINGER-WEBER",
+         bulk("Si", cubic=true) * (12,15,12),
+         StillingerWeber())
