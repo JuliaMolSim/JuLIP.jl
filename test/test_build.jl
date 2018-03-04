008660ed3f5360360122a308160e1dfc024554ac
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-03-04 23:47:51 +0000

    cleanup

---
+++ test/test_build.jl
@@ -0,0 +1,9 @@
+
+
+using Base.Test, JuLIP
+
+println("Testing `jbulk == bulk` ...")
+for sym in [:Fe, :W, :Al, :Cu, :Si, :C, :Mg, :Be]
+   @test isapprox(bulk(sym), Atoms(ASE.bulk(string(sym))), tol = 1e-12)
+   @test isapprox(bulk(sym, cubic=true), Atoms(ASE.bulk(string(sym), cubic=true)), tol = 1e-12)
+end
