e1fbbde4a8b9c4bec95540ab63513dcc1d4929be
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-07-09 14:25:57 +0100

    backup

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,14 +22,14 @@ catch
 end
 
 julip_tests = [
-   ("testaux.jl", "Miscellaneous"),
-   ("test_atoms.jl", "Atoms"),
-   ("test_build.jl", "Build"),
-   ("test_fio.jl", "File IO"),
-   ("testanalyticpotential.jl", "Analytic Potential"),
-   ("testpotentials.jl", "Potentials"),
-   ("test_ad.jl", "AD Potentials"),
-   # ("testvarcell.jl", "Variable Cell"),
+   # ("testaux.jl", "Miscellaneous"),
+   # ("test_atoms.jl", "Atoms"),
+   # ("test_build.jl", "Build"),
+   # ("test_fio.jl", "File IO"),
+   # ("testanalyticpotential.jl", "Analytic Potential"),
+   # ("testpotentials.jl", "Potentials"),
+   # ("test_ad.jl", "AD Potentials"),
+   ("testvarcell.jl", "Variable Cell"),
    # ("testhessian.jl", "Hessian"),
    # ("testsolve.jl", "Solve"),
 ]
