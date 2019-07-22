6ef3efe6a23ee02bc9fe0e08c06e14b3e71fd3dc
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-07-22 03:41:31 +0100

    remember previous performance testes

--- test/performance.jl
+++ test/performance.jl
@@ -42,3 +42,24 @@ perfbm("EAM (Splines)",
 perfbm("STILLINGER-WEBER",
          bulk(:Si, cubic=true) * (12,15,12),
          StillingerWeber())
+
+
+# Performance prior to restructuring 
+# --------------------------------------------------------------------------
+# LENNARD-JONES
+# Energy Assembly (without nlist):   66.695 ms (41653 allocations: 47.65 MiB)
+# Energy Assembly (with nlist):      68.778 ms (41675 allocations: 41.52 MiB)
+# Force Assembly (without nlist):    73.158 ms (41657 allocations: 43.40 MiB)
+# Force Assembly (with nlist):       72.561 ms (41677 allocations: 41.59 MiB)
+# --------------------------------------------------------------------------
+# EAM (Splines)
+# Energy Assembly (without nlist):   69.074 ms (71535 allocations: 43.47 MiB)
+# Energy Assembly (with nlist):      74.438 ms (71558 allocations: 44.49 MiB)
+# Force Assembly (without nlist):    83.091 ms (69237 allocations: 44.23 MiB)
+# Force Assembly (with nlist):       87.327 ms (69257 allocations: 44.51 MiB)
+# --------------------------------------------------------------------------
+# STILLINGER-WEBER
+# Energy Assembly (without nlist):   78.205 ms (402680 allocations: 65.56 MiB)
+# Energy Assembly (with nlist):      86.708 ms (410401 allocations: 69.01 MiB)
+# Force Assembly (without nlist):    87.153 ms (411005 allocations: 67.34 MiB)
+# Force Assembly (with nlist):       91.166 ms (410672 allocations: 69.42 MiB)
