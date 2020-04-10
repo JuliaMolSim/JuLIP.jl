7a43340f47c77c06c1d0361ba49c932dbb9c682c
Christoph Ortner <c.ortner@warwick.ac.uk>
2020-04-10 08:46:56 -0700

    prototype implementation of AD for splines

--- temp/ad_tests.jl
+++ temp/ad_tests.jl
@@ -35,3 +35,65 @@ end
 #
 # @btime f1($R1);
 # @btime f1_rd($R1);
+
+##
+
+module M
+   using ForwardDiff
+   myf(x) = x^2+x+1
+   myg(x) = π   # deliberately incorrect gradient for testing
+
+   function myf(d::ForwardDiff.Dual{T}) where T
+      x = ForwardDiff.value(d)
+      @show d
+      # y = ( myf(x), myg(x) )
+      ForwardDiff.Dual{T}( myf(x), myg(x) * ForwardDiff.partials(d) )
+   end
+end
+
+
+using ForwardDiff
+M.myf(1.0)
+ForwardDiff.derivative(M.myf, 1.0)
+
+
+##
+
+
+module S
+   using ForwardDiff, Dierckx
+
+   modelspline() = Spline1D([0.0, 0.5, 1.0], rand(3); bc = "zero", k = 2)
+   ev(s::Spline1D, x) = s(x)
+   ev_d(s::Spline1D, x) = Dierckx.derivative(s, x, 1)
+   ev_dd(s::Spline1D, x) = Dierckx.derivative(s, x, 2)
+
+   function ev(s::Spline1D, d::ForwardDiff.Dual{T}) where T
+      x = ForwardDiff.value(d)
+      ForwardDiff.Dual{T}( s(x), ev_d(s, x) * ForwardDiff.partials(d) )
+   end
+
+   function ev_d(s::Spline1D, d::ForwardDiff.Dual{T}) where T
+      x = ForwardDiff.value(d)
+      ForwardDiff.Dual{T}( ev_d(s, x), ev_dd(s, x) * ForwardDiff.partials(d) )
+   end
+   # Dierckx.derivative(V.spl, r, nu)
+
+   function compf(s, X)
+      [ sum( ev_d(s, x)^p for x in X )  for p = 0:3 ]
+   end
+
+   function J_compf(s, X)
+      [ (p * ev_d(s, X[i])^(p-1) * ev_dd(s, X[i])) for p=0:3, i=1:length(X) ]
+   end
+end
+
+using ForwardDiff
+s = S.modelspline()
+ForwardDiff.derivative( x -> S.ev(s, x), 0.5) == S.ev_d(s, 0.5)
+ForwardDiff.derivative( x -> S.ev_d(s, x), 0.5) == S.ev_dd(s, 0.5)
+
+X = rand(5)
+J = ForwardDiff.jacobian(X_ -> S.compf(s, X_), X)
+J1 = S.J_compf(s, X)
+J1 ≈ J
