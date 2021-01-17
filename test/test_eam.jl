5020c79b62e3afe9f55446388b7ab6abad866421
James Gardner <james.gardner1421@gmail.com>
2021-01-17 14:56:50 +0000

    Update test file

--- test/test_eam.jl
+++ test/test_eam.jl
@@ -1,6 +1,15 @@
 using Test
+using ASE
 using JuLIP
 
-filename = "data/PdAgH_HybridPd3Ag.eam.alloy"
-@test JuLIP.Potentials.generic_EAM(filename) isa EAM
+eam_alloy = "data/PdAgH_HybridPd3Ag.eam.alloy"
+eam_fs = "data/Fe-P.eam.fs"
+@test JuLIP.Potentials.EAM(eam_alloy) isa EAM{T, 1} where {T}
+@test JuLIP.Potentials.EAM(eam_fs) isa EAM{T, 2} where {T}
 
+eam_Fe = JuLIP.Potentials.EAM1("data/pfe.plt", "data/ffe.plt", "data/F_fe.plt")
+eam_Fe2 = JuLIP.Potentials.EAM("data/pfe.plt", "data/ffe.plt", "data/F_fe.plt")
+
+at = rattle!(bulk(:Fe) * 4, 0.5)
+@test energy(eam_Fe, at) ≈ energy(eam_Fe2, at)
+@test forces(eam_Fe, at) ≈ forces(eam_Fe2, at)
